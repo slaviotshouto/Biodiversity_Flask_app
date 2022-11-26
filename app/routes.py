f62fff23e137da4616b02bea1c1f97b6491aa604
slaviotshouto <svetoslav.ivanov20@gmail.com>
2022-11-26 17:13:08 +0200

    Feat: fit the flask app to the model, add zip and csv functionality

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
-from flask import render_template, request, redirect, jsonify, make_response
+from flask import render_template, request, send_from_directory
 from app.flask_app import app
 import os
+import shutil
+import datetime
+import requests
+import pandas as pd
+
 
 @app.route('/')
 def route():
-    return "Hi there"
+    return render_template('index.html')
 
 
 @app.route('/about')
@@ -19,9 +24,35 @@ def upload():
         if request.files:
 
             # Set as an attribute in the html
-            image = request.files["image"]
-            print(image)
+            images = request.files.getlist('images')
+            temp_folder = os.getcwd() + '\\app\\temp\\'
+            for image in images:
+                image.save(temp_folder + image.filename)
 
-            return redirect(request.url)
+            curr_time = datetime.datetime.now()
+            timestamp = curr_time.timestamp()
+            zip_name = 'temp_{}'.format(str(timestamp))
+
+            shutil.make_archive(zip_name, 'zip', temp_folder)
+
+            # Send over to the model
+            url = 'https://d476-92-247-118-91.eu.ngrok.io/analyze_hook'
+            with open(os.getcwd() + "\\" + zip_name + ".zip", 'rb') as zip_file:
+                req = requests.post(url, files={'file': zip_file.read()})
+                results = req.json()
+                print(results)
+                write_to_csv(results)
+            for file in os.listdir(temp_folder):
+                os.remove(os.path.join(temp_folder, file))
+
+            os.remove(os.getcwd() + "\\" + zip_name + ".zip")
+
+            return send_from_directory(directory=os.getcwd(), path='results.csv', as_attachment=True)
     return render_template('upload.html')
 
+
+def write_to_csv(results_dic):
+    results_list = [[k.capitalize(), v] for k, v in results_dic.items()]
+    print(results_list)
+    df = pd.DataFrame(results_list)
+    df.to_csv('biodiversity_analysis.csv', index=False)
